081be82f1e8133239d6a84f23928119fd6b68ed1
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-02 20:54:59 +0530

    mine

--- readcommand.sh
+++ readcommand.sh
@@ -7,3 +7,9 @@ echo "please enter user name"
 read username # what ever entered in terminal is stored in user name variable
 
 echo "username is $username"
+
+echo "please enter password"
+
+read pass
+
+echo "passwor is $pass"
