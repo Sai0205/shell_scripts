fe2c7d4534978ef761e8ea0e3bccd5c22a434d9e
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-03 21:25:54 +0530

    cond

--- giti.sh
+++ giti.sh
@@ -9,8 +9,14 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "please run with root access"
-    exit = 1
+    exit 1
 fi   
 
 
 yum install git -y
+
+#exit status $? it will store previous command status
+#if $? is not zero previous command failed
+
+#lsss -la fialure
+#ls -la pass 
