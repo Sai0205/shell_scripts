a3796e758cc01f5b81d864cbecfd7a3d51537cee
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-03 21:28:18 +0530

    cond

---
+++ mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#check rootuser or not
+
+#if not root user,exit the program and inform user to run with sudo user
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "please run with root access"
+    exit 1
+fi   
+
+
+yum install mysql -y
+
+#exit status $? it will store previous command status
+#if $? is not zero previous command failed
+
+#lsss -la fialure
+#ls -la pass 
