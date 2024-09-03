8cf27c6b58d53bf090e4bdb97f126b347ab1306a
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-03 21:12:54 +0530

    cond

---
+++ giti.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check rootuser or not
+
+if not root user,exit the program and inform user to run with sudo user
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "please run with root access"
+fi   
+
+
+yum install git -y
