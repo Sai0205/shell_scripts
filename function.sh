8db39f2526e9a15a6741bfc065df0bd0877f493a
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-03 22:00:36 +0530

    cond

---
+++ function.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#dont repeat yourself
+
+
+userid=$(id -u)
+#validate the previous command and inform user it is scucces or failure
+
+VALIDATE(){
+    if [ $1 -ne 0]
+    then
+        echo "failure"
+        exit 1
+    else
+        echo "success"
+
+}
+
+if [ $userid -ne 0]
+then
+    echo "run with sudo access"
+    exit 1
+fi   
+
+yum install mysql -y
+
+VALIDATE $?
+
+yum intsall postfix -y
+
+VALIDATE $?
