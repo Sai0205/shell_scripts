1e85054c85a575cf7838441a283fc4b419563f0b
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-03 14:18:34 +0530

    mo

---
+++ conditions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#conditions
+#if(Expression)
+
+#whyneed oconditions
+
+#algo to install git 
+#install git
+#1.we should be rrot user
+#if [ ]
+#then  
+#    thesecoomand 
+#else
+#    these commands
+#    
+
+num=$1
+
+if [ $num -gt 10]
+then  
+    echo "$num is greater than 10"
+else 
+    echo "not greate"
+fi
