734aeb711296ad75724c0039672a59dec7956046
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-03 22:02:26 +0530

    cond

--- function.sh
+++ function.sh
@@ -7,7 +7,7 @@ userid=$(id -u)
 #validate the previous command and inform user it is scucces or failure
 
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ] 
     then
         echo "failure"
         exit 1
@@ -16,7 +16,7 @@ VALIDATE(){
     fi
 }
 
-if [ $userid -ne 0]
+if [ $userid -ne 0 ]
 then
     echo "run with sudo access"
     exit 1
