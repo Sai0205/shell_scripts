b7e6883f7caad0f8092d597a66c02c668052b2d0
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-09-04 21:58:24 +0530

    modified

--- 14loopsadvanced.sh
+++ 14loopsadvanced.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 userid=$(id -u)
+date=$(date +%F)
+logsdir=/home/centos/shell_scriptlogs
+filename=$0
+logfile=$logsdir/$filename-$date.log
+
 R="\e[31m"
 N="\e[om"
 if [ $userid -ne 0 ]
@@ -9,7 +14,25 @@ then
     exit 1
 fi   
 
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "installing $2 failure"
+        exit 1
+    else
+        echo -e "installing $2 is success"
+        fi
+}
+
 for i in $@
 do
-     yum install $i -y &>>m.log
+    yum list installed $i
+    if [ $? -ne 0]
+    then
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>logfile
+        validate &? "$i"
+    else
+        echo -e "$Y $i is already insatlled $N"
+    fi   
 done  
